f958f49b26838795e656375fb0bca9e9fd6183e7
phirov <phirov@163.com>
2017-02-26 07:49:13 +0000

    update deploy.sh script

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
-IMAGE_NAME='phirov/logstash'
+IMAGE_NAME='phirov/docker-logstash'
 : ${IMAGE_VERSION:='5-patch'}
+
 echo "
     docker rmi ${IMAGE_NAME} ${IMAGE_NAME}:${IMAGE_VERSION}
 "
 docker rmi ${IMAGE_NAME} ${IMAGE_NAME}:${IMAGE_VERSION}
+
 echo "
     docker build -t ${IMAGE_NAME} -t ${IMAGE_NAME}:${IMAGE_VERSION} .
 "
